f053b22c57ae28809dce580a105e96e080bc51cf
XiaomA <805122541@qq.com>
2024-10-03 00:54:50 +0800

    修改启动日志

--- train-member/src/main/java/com/xiaoweii/train/config/MemberApplication.java
+++ train-member/src/main/java/com/xiaoweii/train/config/MemberApplication.java
@@ -1,15 +1,25 @@
 package com.xiaoweii.train.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.xiaoweii")//默认只扫描当前包和子包,所以如果同级的话需要加注释
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        /*
+        这里也可以打印欢迎语句,口号等, 或者项目常用的地址, 比如接口文档,项目文档, 数据库文档等
+        项目的log
+         */
+        LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
     }
-
 }
