e2d86a8bcb5b8f9da53d54727060c209bcdb002f
XiaomA <805122541@qq.com>
2024-10-03 22:11:34 +0800

    使用自定义异常处理异常业务小bug, 将业务异常进行优化处理, 不写入堆栈信息, 提高性能

--- train-common/src/main/java/com/xiaoweii/train/common/exception/BusinessException.java
+++ train-common/src/main/java/com/xiaoweii/train/common/exception/BusinessException.java
@@ -11,4 +11,11 @@ public class BusinessException extends RuntimeException {
 
     private BusinessExceptionEnum e;
 
+    /*
+     * 不写入堆栈信息, 提高性能
+     * */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
