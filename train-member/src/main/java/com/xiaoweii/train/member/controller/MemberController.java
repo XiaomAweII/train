73e30afb8fd2c755eb0c505073219376b50e7d49
XiaomA <805122541@qq.com>
2024-10-03 19:30:55 +0800

    实现一个简单的注册接口, 快捷生成代参的post->ptrp

--- train-member/src/main/java/com/xiaoweii/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/xiaoweii/train/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.xiaoweii.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
 
-//    @Resource
+    //    @Resource
     @Autowired
     private MemberService memberService;
 
     @GetMapping("/count")
-    public long hello(){
+    public long hello() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
--- train-member/src/main/java/com/xiaoweii/train/member/service/MemberService.java
+++ train-member/src/main/java/com/xiaoweii/train/member/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.xiaoweii.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.xiaoweii.train.member.domain.Member;
+import com.xiaoweii.train.member.domain.MemberExample;
 import com.xiaoweii.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -13,5 +18,32 @@ public class MemberService {
 
     public Long count() {
         return memberMapper.countByExample(null);
+    }
+
+    /*
+        注册功能
+     */
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);//createCriteria()相当于生成where条件
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        //Hutool里的工具
+        if (CollUtil.isNotEmpty(list)) {
+            /*
+            方式一:
+            带验证码的注册可以用这种方式, 有验证码, 说明手机号是本人用, 原来注册过的, 就不用再保存了, 直接把数据库返回.
+            这个接口做成既可以是注册, 也可以是登录
+             */
+            //return list.get(0).getId();
+
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());//目前用系统时间戳,后期用雪花算法生成id
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
