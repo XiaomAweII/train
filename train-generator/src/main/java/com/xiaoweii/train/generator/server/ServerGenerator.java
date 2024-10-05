56b62765797877e5f24d5746e2794126ce5305af
XiaomA <805122541@qq.com>
2024-10-06 01:36:50 +0800

    制作DbTtil, 获取表字段信息

--- train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
+++ train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
@@ -1,6 +1,8 @@
 package com.xiaoweii.train.generator.server;
 
 
+import com.xiaoweii.train.generator.util.DbUtil;
+import com.xiaoweii.train.generator.util.Field;
 import com.xiaoweii.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -11,6 +13,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -42,6 +45,18 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        //为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("userId: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
+
         // 示例: 表名 xxx_test
         // Domain = XxxTest
         String Domain = domainObjectName.getText();
@@ -49,6 +64,9 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toUpperCase() + Domain.substring(1);
         // do_main = xxx-test //url命名规范: 全小写, 多个单词用 - 拼接
         String do_main = tableName.getText().replaceAll("_", "-");
+        // 表中文名
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
 
         //组装参数
         Map<String, Object> param = new HashMap<>();
