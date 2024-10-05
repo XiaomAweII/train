a586e83a54554d4b722fecdc36b62f319251d15e
XiaomA <805122541@qq.com>
2024-10-05 23:44:14 +0800

    集成DOM4j, 读取当前持久层的xml文件, 得到表名和实体名

--- train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
+++ train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
@@ -4,6 +4,7 @@ package com.xiaoweii.train.generator.server;
 import com.xiaoweii.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -22,6 +23,23 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws Exception {
+        String generatorPath = getGeneratorPath();
+
+        Document document = new SAXReader().read("train-generator/" + generatorPath);//拼接"train-generator/"模块路径再拼接当前路径
+        Node table = document.selectSingleNode("//table");//两个斜杠表示寻找所有的table节点
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");//@表示查属性
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath + "Test.java", param);
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -31,12 +49,7 @@ public class ServerGenerator {
         //使用XPath快速定位节点或属性;
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
-
-
-//        FreemarkerUtil.initConfig("test.ftl");
-//        HashMap<String, Object> param = new HashMap<>();
-//        param.put("domain", "Test");
-//        FreemarkerUtil.generator(toPath + "Test.java", param);
+        return node.getText();
     }
 
 }
