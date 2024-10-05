d7c709e662d89ace3af9fa0c508e7d2b6a75a869
XiaomA <805122541@qq.com>
2024-10-05 23:37:21 +0800

    集成DOM4j读取pom.xml, 得到当前持久层的xml文件:generator-config-member.xml
    json还未流行时, 数据传递都是用xml, 传递同样的数据, xml包更大, 不利于传输, 主键被淘汰了

--- train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
+++ train-generator/src/main/java/com/xiaoweii/train/generator/server/ServerGenerator.java
@@ -3,24 +3,40 @@ package com.xiaoweii.train.generator.server;
 
 import com.xiaoweii.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerGenerator {
 
     static String toPath = "train-generator/src/main/java/com/xiaoweii/train/generator/test/";
+    static String pomPath = "train-generator/pom.xml";
 
     static {
         new File(toPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", param);
+    public static void main(String[] args) throws Exception {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        //从根目录下寻找,pom是xml命名空间, configurationFile是节点名, 如果要找属性,则是@xxx
+        //使用XPath快速定位节点或属性;
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath + "Test.java", param);
     }
 
 }
