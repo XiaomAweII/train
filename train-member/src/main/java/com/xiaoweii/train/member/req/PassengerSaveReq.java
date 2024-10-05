e9eb0de00f8ad6c88b19da0b8e3f99bd9384b227
XiaomA <805122541@qq.com>
2024-10-05 14:12:14 +0800

    增加乘车人新增的接口, 增加字段非空校验

--- train-member/src/main/java/com/xiaoweii/train/member/req/PassengerSaveReq.java
+++ train-member/src/main/java/com/xiaoweii/train/member/req/PassengerSaveReq.java
@@ -1,17 +1,24 @@
 package com.xiaoweii.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 
 //不止可以给新增使用, 也可以给编辑使用, 所以跟Passenger保持一致
 public class PassengerSaveReq {
     private Long id;
 
+    @NotNull(message = "【会员ID】 不能为空")
     private Long memberId;
 
+    @NotBlank(message = "【名字】 不能为空")
     private String name;
 
+    @NotBlank(message = "【身份照】 不能为空")
     private String idCard;
 
+    @NotBlank(message = "【旅客类型】 不能为空")
     private String type;
 
     private Date createTime;
