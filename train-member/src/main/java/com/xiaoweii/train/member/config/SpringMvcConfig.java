1f570877bb9c389e86a3dfc907547b01a324eef5
XiaomA <805122541@qq.com>
2024-10-05 14:58:18 +0800

    增加日志拦截器, 统一打印日志流水号, 解决之前用户拦截器没有日志流水号的问题

--- train-member/src/main/java/com/xiaoweii/train/member/config/SpringMvcConfig.java
+++ train-member/src/main/java/com/xiaoweii/train/member/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.xiaoweii.train.member.config;
 
+import com.xiaoweii.train.common.interceptor.LogInterceptor;
 import com.xiaoweii.train.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,14 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     @Resource
     MemberInterceptor memberInterceptor;
 
+    @Resource
+    LogInterceptor logInterceptor;
+
     //添加拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);//启用拦截器
+
         // 路径不要包含context-path
         registry.addInterceptor(memberInterceptor)
                 .addPathPatterns("/**") //正则表达式, 针对所有请求接口生效
