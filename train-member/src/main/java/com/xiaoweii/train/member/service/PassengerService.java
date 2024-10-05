4794d30fe3802730b3bf4c0392bb093cc1dcc4e3
XiaomA <805122541@qq.com>
2024-10-05 17:13:42 +0800

    集成PageHelper, 实现sql分页功能

--- train-member/src/main/java/com/xiaoweii/train/member/service/PassengerService.java
+++ train-member/src/main/java/com/xiaoweii/train/member/service/PassengerService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.github.pagehelper.PageHelper;
 import com.xiaoweii.train.common.context.LoginMemberContext;
 import com.xiaoweii.train.common.util.SnowUtil;
 import com.xiaoweii.train.member.domain.Passenger;
@@ -44,6 +45,7 @@ public class PassengerService {
         if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        PageHelper.startPage(2, 2);
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
         return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
 
