4baa6e64adc6ab06a4a38b2661518f0a570a986a
XiaomA <805122541@qq.com>
2024-10-06 21:36:28 +0800

    修改admin模块, 去掉登录界面, 去掉member和登录相关的拦截, 去掉乘车人界面

--- admin/src/components/HeaderComponent.vue
+++ admin/src/components/HeaderComponent.vue
@@ -2,11 +2,7 @@
   <a-layout-header class="header">
     <div class="logo"/>
     <div style="float: right;color: white;">
-      您好: {{ member.mobile }} &nbsp;&nbsp;
-      <!-- 使用router-link标签+to来跳转页面, 相当于a标签+href-->
-      <router-link to="/login" style="color: white">
-        退出登录
-      </router-link>
+      欢迎使用登录控台
     </div>
     <a-menu
         v-model:selectedKeys="selectedKeys"
@@ -19,9 +15,9 @@
           <coffee-outlined/> &nbsp; 欢迎
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined/> &nbsp; 乘车人管理
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined/> &nbsp; 关于
         </router-link>
       </a-menu-item>
     </a-menu>
@@ -30,16 +26,11 @@
 
 <script>
 import {defineComponent, ref, watch} from 'vue';
-import store from "@/store";
 import router from '@/router';
 
 export default defineComponent({
   name: "HeaderComponent",
   setup() {
-    /*
-    * 因为header只是显示, 不会改member, 所以声明成普通变量就可以, 不需要响应式变量
-    * */
-    let member = store.state.member;
     const selectedKeys = ref([]);
 
     watch(() => router.currentRoute.value.path, (newValue) => {
@@ -49,7 +40,6 @@ export default defineComponent({
     }), {immediate: true};
 
     return {
-      member,
       selectedKeys
     };
   },
