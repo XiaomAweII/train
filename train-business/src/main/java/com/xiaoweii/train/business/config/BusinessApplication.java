b83f3ea8fc6c999dc8c7606549f85ed8d83766fd
XiaomA <805122541@qq.com>
2024-10-06 22:03:47 +0800

    增加business业务模块, 所有关于车次的维护, 卖票功能都会放在这个模块里, 这一流程还是很重要, 以后新增模块都按这里的修改来

---
+++ train-business/src/main/java/com/xiaoweii/train/business/config/BusinessApplication.java
@@ -0,0 +1,27 @@
+package com.xiaoweii.train.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.xiaoweii")//不能写的太通用, 比如只写了个"com",这样会把第三方的类也扫描到, 会有问题
+@MapperScan("com.xiaoweii.train.*.mapper")//添加mapper扫描
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        /*
+        这里也可以打印欢迎语句,口号等, 或者项目常用的地址, 比如接口文档,项目文档, 数据库文档等
+        项目的log
+         */
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+    }
+}
